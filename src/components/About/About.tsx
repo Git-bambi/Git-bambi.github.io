1cc8b33be6bd4fa56c9aca4bbd6066aef5448c19
git-bambi <m.wojdak881@gmail.com>
2024-05-17 23:53:17 +0200

    add about info

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,6 +1,13 @@
 const About = () => {
   return (
-    <div>Informacje o stronie którą napisał Miłosz</div>
+    <div>
+      <h2>Autor projektu:</h2>
+      <h1>Miłosz Wojdak</h1>
+      <p>
+        Projekt strony internetowej wykonany w ramach
+        zaliczenia przedmiotu MO: TIiSKwE na kierunku Energetyka.
+      </p>
+    </div>
   )
 }
 
