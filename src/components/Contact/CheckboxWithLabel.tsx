307ca6565d995bb7eab2b163fa5abc94981b7e8a
git-bambi <m.wojdak881@gmail.com>
2024-05-18 13:32:09 +0200

    Prepapre calculator

--- src/components/Contact/CheckboxWithLabel.tsx
+++ src/components/Contact/CheckboxWithLabel.tsx
@@ -2,12 +2,13 @@ import { Checkbox } from "@/components/ui/checkbox"
 
 interface CheckboxWithLabelProps {
   text: string;
+  onChange?: (value: boolean) => void;
 }
 
 export function CheckboxWithLabel(props: CheckboxWithLabelProps) {
   return (
     <div className="m-1 flex items-center space-x-2">
-      <Checkbox id="terms"  />
+      <Checkbox onCheckedChange={props.onChange} id="terms"  />
       <label
         htmlFor="terms"
         className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
